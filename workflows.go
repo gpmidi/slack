3742c3ab62c2eb69bab35b25f8b9b3a93803ac09
Paulson McIntyre <pmcintyre@salesforce.com>
2021-09-14 14:41:00 -0400

    Changed over to use JSON posts

--- workflows.go
+++ workflows.go
@@ -2,66 +2,65 @@ package slack
 
 import (
 	"context"
-	"encoding/json"
-	"net/url"
 )
 
-// https://api.slack.com/reference/workflows/workflow_step#input
+// Workflow interaction events that can be triggered by Slack
+const (
+	WorkflowEventStepExec    = "workflow_step_execute"
+	WorkflowEventStepEdit    = "workflow_step_edit"
+	WorkflowEventUnpublished = "workflow_unpublished"
+	WorkflowEventStepDeleted = "workflow_step_deleted"
+	WorkflowEventPublished   = "workflow_published"
+	WorkflowEventDeleted     = "workflow_deleted"
+)
+
+// WorkflowInput - https://api.slack.com/reference/workflows/workflow_step#input
 type WorkflowInput struct {
 	Value                   string            `json:"value,omitempty"`
 	Variables               map[string]string `json:"variables,omitempty"`
 	SkipVariableReplacement bool              `json:"skip_variable_replacement"`
 }
 
-// https://api.slack.com/reference/workflows/workflow_step#output
+// WorkflowOutput - https://api.slack.com/reference/workflows/workflow_step#output
 type WorkflowOutput struct {
-	Name  string
-	Type  string
-	Label string
+	Name  string `json:"name"`
+	Type  string `json:"type"`
+	Label string `json:"label"`
 }
 
-type WorkflowRef struct {
-	StepName     string
-	StepImageURL string
-	Outputs      []*WorkflowOutput
-	Inputs       map[string]*WorkflowInput
+// WorkflowRequest - https://api.slack.com/methods/workflows.updateStep#args
+type WorkflowRequest struct {
+	TriggerID    string                    `json:"workflow_step_edit_id"`
+	StepName     string                    `json:"step_name"`
+	StepImageURL string                    `json:"step_image_url"`
+	Outputs      []*WorkflowOutput         `json:"outputs"`
+	Inputs       map[string]*WorkflowInput `json:"inputs"`
 }
 
-func (api *Client) WorkflowUpdateStep(channel string, item ItemRef) error {
-	return api.AddStarContext(context.Background(), channel, item)
+//NewWorkflowRequest create a new workflow request object. Requires a trigger_id from a ViewRequest object that was
+// sent as part of a `workflow_step_execute` type interaction.
+func NewWorkflowRequest(triggerID string) WorkflowRequest {
+	return WorkflowRequest{
+		TriggerID:    triggerID,
+		StepName:     "",
+		StepImageURL: "",
+		Outputs:      []*WorkflowOutput{},
+		Inputs:       map[string]*WorkflowInput{},
+	}
 }
 
+// WorkflowUpdateStep notifies Slack that the workflow update in question has been handled
+func (api *Client) WorkflowUpdateStep(request WorkflowRequest) error {
+	return api.WorkflowUpdateStepContext(context.Background(), request)
+}
+
+// WorkflowUpdateStepContext notifies Slack that the workflow update in question has been handled
 // https://api.slack.com/methods/workflows.updateStep
-func (api *Client) WorkflowUpdateStepContext(ctx context.Context, workflowStepEditID string, wf WorkflowRef) error {
-	values := url.Values{
-		"workflow_step_edit_id": {workflowStepEditID},
-		"token":                 {api.token},
-	}
-	if wf.StepName != "" {
-		values.Set("step_name", wf.StepName)
-	}
-	if wf.StepImageURL != "" {
-		values.Set("step_image_url", wf.StepImageURL)
-	}
-	if wf.Inputs != nil {
-		data, err := json.Marshal(wf.Inputs)
-		if err != nil {
-			return err
-		}
-		values.Set("inputs", (string)(data))
-	}
-	if wf.Outputs != nil {
-		data, err := json.Marshal(wf.Outputs)
-		if err != nil {
-			return err
-		}
-		values.Set("outputs", (string)(data))
-	}
+func (api *Client) WorkflowUpdateStepContext(ctx context.Context, request WorkflowRequest) error {
 
 	response := &SlackResponse{}
-	if err := api.postMethod(ctx, "workflows.updateStep", values, response); err != nil {
+	if err := api.postJSON(ctx, "workflows.updateStep", request, response); err != nil {
 		return err
 	}
-
 	return response.Err()
 }
